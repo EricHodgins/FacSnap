edfa7bc4bf973d8fca55b47d7583afc3f9cf49b8
Eric Hodgins <hodgins.e@gmail.com>
2016-10-04 23:21:00 -0400

    feat: Adding tags to the photo.

--- FaceSnap/PhotoMetadataController.swift
+++ FaceSnap/PhotoMetadataController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class PhotoMetadataController: UITableViewController {
     
@@ -36,10 +37,37 @@ class PhotoMetadataController: UITableViewController {
         
         return screenWidth * imgFactor
     }()
+    
+    lazy var locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tap to add location"
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isHidden = true
+        return view
+    }()
+    
+    lazy var tagsTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "summer, vaction"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    var locationManager: LocationManager!
+    var location: CLLocation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(PhotoMetadataController.savePhotoWithMetadata))
+        navigationItem.rightBarButtonItem = saveButton
     }
 
 
@@ -71,6 +99,30 @@ extension PhotoMetadataController {
                 photoImageView.leftAnchor.constraint(equalTo: cell.contentView.leftAnchor)
             ])
             
+        case (1, 0):
+            cell.contentView.addSubview(locationLabel)
+            cell.contentView.addSubview(activityIndicator)
+            
+            NSLayoutConstraint.activate([
+                activityIndicator.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+                activityIndicator.leftAnchor.constraint(equalTo: cell.contentView.leftAnchor, constant: 20),
+                
+                locationLabel.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                locationLabel.rightAnchor.constraint(equalTo: cell.contentView.rightAnchor, constant: 16.0),
+                locationLabel.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+                locationLabel.leftAnchor.constraint(equalTo: cell.contentView.leftAnchor, constant: 20.0)
+                ])
+            
+        case (2, 0):
+            cell.contentView.addSubview(tagsTextField)
+            
+            NSLayoutConstraint.activate([
+                tagsTextField.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                tagsTextField.rightAnchor.constraint(equalTo: cell.rightAnchor, constant: 16.0),
+                tagsTextField.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+                tagsTextField.leftAnchor.constraint(equalTo: cell.contentView.leftAnchor, constant: 20.0)
+            ])
+            
         default:
             break
         }
@@ -79,6 +131,30 @@ extension PhotoMetadataController {
         return cell
     }
 }
+
+// MARK: - Helper Methods
+
+extension PhotoMetadataController {
+    func tagsFromTextField() -> [String] {
+        guard let tags = tagsTextField.text else { return [] }
+        
+        let commaSeparatedSubSequences = tags.characters.split { $0 == "," }
+        let commaSeparatedStrings = commaSeparatedSubSequences.map(String.init)
+        let lowercaseTags = commaSeparatedStrings.map { $0.lowercased() }
+        
+        return lowercaseTags.map { $0.trimmingCharacters(in: NSCharacterSet.whitespaces) }
+    }
+}
+
+
+// MARK: - Persistence
+
+extension PhotoMetadataController {
+    func savePhotoWithMetadata() {
+        
+    }
+}
+
 
 // MARK: - UITableViewDelegate 
 
@@ -89,6 +165,41 @@ extension PhotoMetadataController {
             return imageViewHeight
         default:
             return tableView.rowHeight
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (1, 0):
+            locationLabel.isHidden = true
+            activityIndicator.isHidden = false
+            activityIndicator.startAnimating()
+            
+            locationManager = LocationManager()
+            locationManager.onLocationFix = { placemark, error in
+                if let placemark = placemark {
+                    self.location = placemark.location!
+                    
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.isHidden = true
+                    self.locationLabel.isHidden = false
+                    
+                    guard let name = placemark.name, let city = placemark.locality, let area = placemark.administrativeArea else { return }
+                    
+                    self.locationLabel.text = "\(name), \(city), \(area)"
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Photo"
+        case 1: return "Enter a location"
+        case 2: return "Enter tags"
+        default: return nil
         }
     }
 }
